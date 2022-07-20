4379f2f648acb3799852a631ce0f75c810e9a185
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-07-19 23:53:32 -0300

    Create ContadorComFor.java

---
+++ ContadorComFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class ContadorComFor {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Digite o numero de inicio do contador: ");
+        int n1 = s.nextInt();
+
+        System.out.print("Digite o numero de fim do contador: ");
+        int n2 = s.nextInt();
+
+        System.out.print("Digite o passo contador: ");
+        int p = s.nextInt();
+
+        for(int i = n1; n1<=n2; n1+=p){
+            System.out.println(n1);
+        }
+
+    }
+}
