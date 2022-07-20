bd57f7395a4c9cd0c4826532c9fa2aab2bf547b9
Heitor Barreto Bonfim <105394865+HectorBrrt@users.noreply.github.com>
2022-07-19 23:52:48 -0300

    Update Fatorial.java

--- Fatorial.java
+++ Fatorial.java
@@ -8,11 +8,21 @@ public class Fatorial {
         int n = numero.nextInt();
         int i = 2;
         int fat = 1;
+        int j = n;
 
         while( i <=n ){
             fat = fat * i;
             i++;
         }
-        System.out.printf("Fatorial de %d = %d", n, fat);
+        if(n == 0 || n == 1){
+            System.out.printf("Fatorial de %d = %d", n, fat);
+        }
+        else{
+            while (j != 1) {
+                System.out.printf("%d x ", j);
+                j--;
+            }
+            System.out.printf("1 = %d", fat);
+        }
     }
 }
